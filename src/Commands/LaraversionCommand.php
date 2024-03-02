8e7264a64a0a505ba1d6324cc224226e26545d68
Barry <barrylaouanmagagi@gmail.com>
2024-03-02 23:11:48 +0100

    Update namespace in LaraversionCommand.php

--- src/Commands/LaraversionCommand.php
+++ src/Commands/LaraversionCommand.php
@@ -4,7 +4,7 @@ namespace Laraversion\Laraversion\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use App\Models\VersionHistory;
+use Laraversion\Laraversion\Models\VersionHistory;
 
 class LaraversionCommand extends Command
 {
